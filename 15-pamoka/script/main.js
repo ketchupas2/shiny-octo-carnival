78823a4620b0df4136fba0b6135ad23a7e19e4f2
ketchupas2 <123992994+ketchupas2@users.noreply.github.com>
2023-03-07 18:20:59 +0200

    Delete 15-pamoka directory

--- 15-pamoka/script/main.js
+++
@@ -1,8 +0,0 @@
-const visual = document.querySelector('#visual');
-
-window.addEventListener('mousemove', function(event){
-    // console.log('paspaudimas');
-    // console.log(event.pageX, event.pageY);
-
-    visual.style.transform = `translate(${event.pageX/100}px, ${event.pageY/100}px)`;
-});
